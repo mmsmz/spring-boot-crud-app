e9ecfa6d52c5cf7f91fe19a07067735f89352964
Mohamed Arafath <35114431+mmsmz@users.noreply.github.com>
2025-01-21 20:24:56 +0800

    initial commit

---
+++ src/main/java/com/project/crud_applicaion/CrudApplicaionApplication.java
@@ -0,0 +1,13 @@
+package com.project.crud_applicaion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudApplicaionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudApplicaionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/crud_applicaion/CrudApplicaionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.crud_applicaion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicaionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
